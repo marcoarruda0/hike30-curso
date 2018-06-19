281ae70f111344f4642d2e898666be87b4f23454
Marco Arruda <marcoarruda0@gmail.com>
2018-06-19 17:47:17 -0300

    Criar template de contato

---
+++ rede-LZAfranquias/lib/router.js
@@ -0,0 +1,19 @@
+FlowRouter.route("/", {
+    action: function(params, queryParams) {
+        BlazeLayout.render('LayoutPrincipal', {main:'Inicio'});
+    }
+});
+
+
+FlowRouter.route("/sobre", {
+    action: function(params, queryParams) {
+        BlazeLayout.render('LayoutPrincipal', {main:'Sobre'});
+    }
+});
+
+
+FlowRouter.route("/contato", {
+    action: function(params, queryParams) {
+        BlazeLayout.render('LayoutPrincipal', {main:'Contato'});
+    }
+});
